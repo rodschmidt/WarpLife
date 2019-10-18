148b37dd15e2fe7e4c89cd4820aa66d3c18a3773
rodschmidt <rod_schmidt@hotmail.com>
2019-10-18 02:31:02 -0700

    Add files via upload

---
+++ Build/Intermediates/PrecompiledHeaders/Warp_Life_Prefix-aigrtydecvtkimfssvttyfabodgv/Warp_Life_Prefix.pch.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/mike/Source/Warp_Life/Warp_Life/trunk/Warp_Life_Prefix.pch \
+  /Applications/Xcode_8.3.3/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator10.3.sdk/usr/include/Availability.h \
+  /Applications/Xcode_8.3.3/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator10.3.sdk/usr/include/AvailabilityInternal.h
---
+++ Build/Intermediates/PrecompiledHeaders/Warp_Life_Prefix-arfrpmcppiisqjfsrayzzdcapegm/Warp_Life_Prefix.pch.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/mike/Source/Warp_Life/Warp_Life/trunk/Warp_Life_Prefix.pch \
+  /Applications/Xcode_8.3.3/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS10.3.sdk/usr/include/Availability.h \
+  /Applications/Xcode_8.3.3/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS10.3.sdk/usr/include/AvailabilityInternal.h

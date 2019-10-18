b9fb37811c7fc0a886b3c9de7259da76a158536f
rodschmidt <rod_schmidt@hotmail.com>
2019-10-18 02:44:52 -0700

    Add files via upload

---
+++ Build/Intermediates/Warp Life.build/Debug-iphoneos/Warp Life.build/Objects-normal/CreditViewControllerIPad.d
@@ -0,0 +1,5 @@
+dependencies: \
+  /Users/mike/Source/Warp_Life/Warp_Life/trunk/Warp_Life_Prefix.pch \
+  /Users/mike/Source/Warp_Life/Warp_Life/trunk/Classes/CreditViewControllerIPad.m \
+  /Users/mike/Source/Warp_Life/Warp_Life/trunk/Classes/CreditViewControllerIPad.h \
+  /Users/mike/Source/Warp_Life/Warp_Life/trunk/Classes/CreditViewControllerIOS.h

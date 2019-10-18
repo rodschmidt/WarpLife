288ce0f3b789a9b9c1566a796ab07fe6a197c0c9
rodschmidt <rod_schmidt@hotmail.com>
2019-10-18 02:47:05 -0700

    Add files via upload

---
+++ Build/Intermediates/Warp Life.build/Debug-iphoneos/Warp Life.build/Objects-normal/arm64/AccelerometerHelper.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/mike/Source/Warp_Life/Warp_Life/trunk/Warp_Life_Prefix.pch \
+  /Users/mike/Source/Warp_Life/Warp_Life/trunk/iPhoneDevelopersCookbook/AccelerometerHelper.m \
+  /Users/mike/Source/Warp_Life/Warp_Life/trunk/iPhoneDevelopersCookbook/AccelerometerHelper.h
